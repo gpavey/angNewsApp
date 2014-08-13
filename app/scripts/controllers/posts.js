5033cf57c3bea1fed3ec975bb53577df85d96d4f
Geoff Pavey <geoff.pavey@gmail.com>
2014-08-13 15:29:29 -0700

    crud working with firebase

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,18 +1,19 @@
 'use strict';
-app.controller("PostsController", function($scope,Post){
-  $scope.posts =[];
+
+app.controller("PostsController", function($scope, Post){
+  $scope.posts = Post.get();
 
   $scope.post = {url: 'http://', title: ''};
 
   $scope.submitPost = function(){
-    Post.save($scope.post, function(ref){
+    Post.save($scope.post, function (ref) {
       $scope.posts[ref.name] = $scope.post
       $scope.post = {url: 'http://', title: ''};
     });
   };
 
   $scope.deletePost = function(postId){
-    Post.delete({id: postID}, function(){
+    Post.delete({id: postId}, function(){
       delete $scope.post[postId]
     });
   };
--- app/scripts/services/post.js
+++ app/scripts/services/post.js
@@ -1,5 +1,5 @@
 "use strict";
 
-app.factory("Post", function($resource){
+app.factory('Post', function($resource){
   return $resource('https://amber-torch-3049.firebaseio.com/posts/:id.json');
 });
